830f5e90d4c52d54c796ff91c3473e707579df22
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-06 00:14:41 +0900

    useStateでフォーム追加

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [task, newTask] = useState("");
+  const addTask = (e) => newTask(e.target.value);
+
   return (
     <div className="main">
       <h1>TO-DO LIST</h1>
       <form id="add">
+        <input placeholder="new task" value={task} onChange={addTask} />
         <button>
           <strong>ADD</strong>
         </button>
       </form>
-
       <div class="tasksBoard">
         <ul id="todo-list"></ul>
         <a id="clear">Clear</a>
