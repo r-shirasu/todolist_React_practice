0e3024e2368adfe9d3e0039da9f13660c2e05dd2
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-08 16:28:22 +0900

    addTaskイベントを設定

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import "./App.scss";
 
 function App() {
   const [task, newTask] = useState("");
-  const addTask = (e) => newTask(e.target.value);
+
+  const addTask = (e) => {
+    newTask(e.target.value);
+    console.log("クリック");
+    e.preventDefault();
+  };
 
   return (
     <div className="main">
       <h1>TO-DO LIST</h1>
       <form id="add">
-        <input placeholder="new task" value={task} onChange={addTask} />
-        <button className="btn">ADD</button>
+        <input
+          type="text"
+          placeholder="new task"
+          value={task}
+          onChange={addTask}
+        />
+        <button onClick={addTask}>ADD</button>
       </form>
       <div className="tasksBoard">
         <ul id="todo-list"></ul>
