9385e01b74efddec871eba07d85e8a27d2b3de65
shirasu-aq <82349387+shirasu-aq@users.noreply.github.com>
2021-06-15 12:14:34 +0900

    Todoリスト開発用ブランチ (#2)
    * HTML修正
    
    * scssの導入
    
    * scss追加
    
    * useStateでフォーム追加
    
    * フォームにcss追加
    
    * aタグをpタグに、classをclassNameに変更
    
    * addButtonのクラスと要素を変更
    
    * addTaskイベントを設定
    
    * 登録したタスクをliに表示
    
    * keyを設定
    
    * todosにtaskを追加した後、テキストフォームを空にする
    
    * uniqueキーを設定
    
    * アロー関数に書き換え、named exportに変更
    
    * 削除ボタン実装
    
    * チェックボタンの実装
    
    * clearAction（イベント）追加
    
    * クリア機能実装
    
    * 削除ボタンをクリックで特定のTodoを削除する (#7)
    
    * deleteAction(イベント)追加
    
    * 'deleteArr'実装
    
    * （質問）削除機能の修正 (#8)
    
    * value, indexが消せない
    
    * filter関数の中身を修正
    
    * Todoにチェックすると要素が変わる (#9)
    
    * チェックした後に要素をつける
    
    * 要素にcolor赤を追加
    
    * 特定のTodoをチェック状態にする
    
    * 全てのチェックボックスが連動されてしまう
    
    * 前の状態に戻す
    
    * 連動が解除できない
    
    * concatでtodosにオブジェクト追加, valueにも命名
    
    * フォームに文字が入力されていないとfalseになる (#11)
    
    * レビュー後の修正箇所まとめ (#12)
    
    * useStateの2番目の戻り値の名前をset~に変更
    
    * setTask("")の位置を変更（処理が終わってからフォームを空にする）
    
    * return falseを削除し、if (task !== "") も削除
    
    * 値を改めて代入せずにtodoだけ返す
    
    * buttonをinputに変更
    
    * clearのidを削除
    
    * 空文字入力の時のメッセージをアラートからHTML要素に変更
    
    * formでonSubmitを活用 (#13)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.scss";
 
-function App() {
+export const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isShowAlertMessage, setIsShowMessage] = useState(false);
+
+  const addTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (task === "") {
+      setIsShowMessage(true);
+      return;
+    }
+
+    setIsShowMessage(false);
+    setTodos(todos.concat({ task: task, isChecked: false }));
+    setTask("");
+  };
+
+  const handleCheck = (index) => {
+    const checkedTodos = todos.map((todo, _index) => {
+      if (_index !== index) {
+        return todo;
+      }
+      return {
+        task: todo.task,
+        isChecked: !todo.isChecked,
+      };
+    });
+    setTodos(checkedTodos);
+  };
+
+  const clearAction = () => {
+    setTodos([]);
+  };
+
+  const deleteAction = (index) => {
+    const deleteArr = todos.filter((_, id) => {
+      return id !== index;
+    });
+    setTodos(deleteArr);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>TO-DO LIST</h1>
+      <form onSubmit={handleClick} id="add">
+        <input
+          type="text"
+          placeholder="new task"
+          value={task}
+          onChange={addTask}
+        />
+        <input type="submit" value="ADD" />
+      </form>
+      {isShowAlertMessage && (
+        <div className="alertMessage">Todoを入力してください</div>
+      )}
+      <div className="tasksBoard">
+        <ul id="todo-list">
+          {todos.map((todo, index) => (
+            <li key={`${todo}${index}`}>
+              <span onClick={() => deleteAction(index)}>×</span>
+
+              <label name={index} className={todo.isChecked ? "checked" : ""}>
+                <input
+                  type="checkbox"
+                  checked={todo.isChecked}
+                  name="check"
+                  onChange={() => handleCheck(index)}
+                />
+                {todo.task}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <p onClick={clearAction}>Clear</p>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
