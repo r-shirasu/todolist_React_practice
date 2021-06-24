73fd62e08580a5269bda6ee5135cb2931ffdb8da
tatsuya-asami <tatsuya.asami@asia-quest.jp>
2021-06-24 14:07:57 +0900

    マークアップ以外削除

--- src/App.js
+++ src/App.js
@@ -1,87 +1,26 @@
 import React from "react";
-import { useState } from "react";
 import "./App.scss";
 
 export const App = () => {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [isShowAlertMessage, setIsShowMessage] = useState(false);
-
-  const addTask = (e) => {
-    setTask(e.target.value);
-  };
-
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    if (task === "") {
-      setIsShowMessage(true);
-      return;
-    }
-
-    setIsShowMessage(false);
-    setTodos(todos.concat({ task: task, isChecked: false }));
-    setTask("");
-  };
-
-  const handleCheck = (index) => {
-    const checkedTodos = todos.map((todo, _index) => {
-      if (_index !== index) {
-        return todo;
-      }
-      return {
-        task: todo.task,
-        isChecked: !todo.isChecked,
-      };
-    });
-    setTodos(checkedTodos);
-  };
-
-  const clearAction = () => {
-    setTodos([]);
-  };
-
-  const deleteAction = (index) => {
-    const deleteArr = todos.filter((_, id) => {
-      return id !== index;
-    });
-    setTodos(deleteArr);
-  };
-
   return (
     <div className="main">
       <h1>TO-DO LIST</h1>
-      <form onSubmit={handleClick} id="add">
-        <input
-          type="text"
-          placeholder="new task"
-          value={task}
-          onChange={addTask}
-        />
+      <form id="add">
+        <input type="text" placeholder="new task" />
         <input type="submit" value="ADD" />
       </form>
-      {isShowAlertMessage && (
-        <div className="alertMessage">Todoを入力してください</div>
-      )}
       <div className="tasksBoard">
         <ul id="todo-list">
-          {todos.map((todo, index) => (
-            <li key={`${todo}${index}`}>
-              <span onClick={() => deleteAction(index)}>×</span>
+          <li>
+            <span>×</span>
 
-              <label name={index} className={todo.isChecked ? "checked" : ""}>
-                <input
-                  type="checkbox"
-                  checked={todo.isChecked}
-                  name="check"
-                  onChange={() => handleCheck(index)}
-                />
-                {todo.task}
-              </label>
-            </li>
-          ))}
+            <label>
+              <input type="checkbox" name="check" />
+              task
+            </label>
+          </li>
         </ul>
-        <p onClick={clearAction}>Clear</p>
+        <p>Clear</p>
       </div>
     </div>
   );
