b59fa675a0c5cc123f8128cac84631627c2bb845
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-01 20:40:13 +0900

    Config.jsを削除

--- src/Config.js
+++
@@ -1,7 +0,0 @@
-export const Config = () => {
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3004/todoList";
-  } else if (process.env.NODE_ENV === "production") {
-    return "http://localhost:3004/todoList";
-  }
-};
